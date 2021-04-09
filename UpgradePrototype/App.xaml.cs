0a5539c2c6dfd4a7d3445e514896a1cc5ae31452
savaged <david.savage@assure-consulting.co.uk>
2021-04-09 11:57:56 +0100

    init from within Rider

---
+++ UpgradePrototype/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using UpgradePrototype.Views;
+
+namespace UpgradePrototype
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            Current.MainWindow = new MainWindow();
+            Current.MainWindow.Show();
+        }
+    }
+}
